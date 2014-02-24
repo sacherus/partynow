f3f065868192744947323951498f8decdda647fb
Elwira Lichocka <ariwle.lichocka@gmail.com>
2014-02-24 19:19:04 +0100

    elwirka

--- src/com/sacherus/partynow/pojos/Party.java
+++ src/com/sacherus/partynow/pojos/Party.java
@@ -1,13 +1,15 @@
 package com.sacherus.partynow.pojos;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.content.ContentValues;
 
 import com.sacherus.partynow.provider.PartiesContract;
 import com.sacherus.partynow.provider.PartiesContract.PartyColumnHelper;
-import com.sacherus.partynow.rest.RestApi;
 import com.sacherus.utils.Utils;
 
 public class Party implements Serializable {
@@ -106,11 +108,47 @@ public class Party implements Serializable {
 	public void setParticipants(List<Integer> participants) {
 		this.participants = participants;
 	}
+	
+	private GregorianCalendar getStartDate() {
+		Calendar cal = GregorianCalendar.getInstance();
+		cal.setTime(Utils.stringToDate(start)); 
+		return (GregorianCalendar)cal;
+	}
+	
+	private GregorianCalendar getEndDate() {
+		Calendar cal = GregorianCalendar.getInstance();
+		cal.setTime(Utils.stringToDate(end)); 
+		return (GregorianCalendar)cal;
+	}
+	
+	public int getStartDay() {
+		return getStartDate().get(Calendar.DAY_OF_MONTH);
+	}
+	
+	public int getStartMonth() {
+		return getStartDate().get(Calendar.MONTH);
+	}
+	
+	public int getStartYear(){
+		return getStartDate().get(Calendar.YEAR);
+	}
+	
+	public int getEndDay() {
+		return getEndDate().get(Calendar.DAY_OF_MONTH);
+	}
+	
+	public int getEndMonth() {
+		return getEndDate().get(Calendar.MONTH);
+	}
+	
+	public int getEndYear(){
+		return getEndDate().get(Calendar.YEAR);
+	}
 
 	public String toString() {
 		return title;
 	}
-
+	
 	public ContentValues toContent() {
 		ContentValues cv = new ContentValues();
 		cv.put(PartyColumnHelper.TITLE, getTitle());
@@ -164,6 +202,16 @@ public class Party implements Serializable {
 		
 		public PartyBuilder addTitle(String v) {
 			instance.title = v;
+			return this;
+		}
+		
+		public PartyBuilder addStartDate(String v) {
+			instance.start = v;
+			return this;
+		}
+		
+		public PartyBuilder addEndDate(String v) {
+			instance.end = v;
 			return this;
 		}
 		
